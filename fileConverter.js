71d28b2b81965f4a3e28e83fe5e136933576c961
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-11-10 12:08:15 +0600

    report json created

---
+++ fileConverter.js
@@ -0,0 +1,29 @@
+import * as exec from "child_process";
+import * as fs from "fs";
+
+export const convertMp4ToGif = async (fileName, newFileName, frameRate, startTime = 0, duration = 0) => {
+    return new Promise(async (resolve, reject) => {
+      newFileName = newFileName.replace(".mp4", ".gif");
+      newFileName = newFileName.replace(".MOV", ".gif");
+      newFileName = newFileName.replace(".webm", ".gif");
+      console.log("new filename => ", newFileName, fileName);
+      try {
+        if (fs.existsSync(`${newFileName}`)) {
+          fs.unlinkSync(`${newFileName}`);
+        }
+
+        const result = exec.execSync(
+            `ffmpeg -ss ${startTime} -t ${duration} -i ${fileName} -vf "fps=${frameRate},scale=500:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ${newFileName} `,
+            ).toString();
+            
+        resolve(result);
+
+      } catch (err) {
+        reject(err);
+      }
+
+    }).catch((error) => {
+      console.log('convertMp4ToGif error: ', error)
+      throw error;
+    });
+  }
--- main.js
+++ main.js
@@ -4,15 +4,18 @@ import * as fs from 'fs';
 import { getFilesFromDir } from './getFilesFromDir.js';
 import { getFileSize } from './fileSize.js';
 import { getFramePerSecondFromFileName } from './fpsCount.js';
+import { convertMp4ToGif } from './fileConverter.js';
 import { alphNumericName } from './generateUniqueId.js';
 
 // media file directory
 const fileDir = '/home/rajibhasan/Desktop/gifbuz-tump/MovFile';
-
+const baseDir = '/home/rajibhasan/Desktop/gifbuz-tump';
 
 // Main Function
 const mainFunction = async () => {
     try {
+        const result = [];
+
         const files = await getFilesFromDir(fileDir);
         files.forEach(async(fileName, index) => {
             // const newName = alphNumericName() + '.MOV';
@@ -20,9 +23,27 @@ const mainFunction = async () => {
 
             const fps = await getFramePerSecondFromFileName(`${fileDir}/${fileName}`);
             const size = await getFileSize(`${fileDir}/${fileName}`);
-            console.log(`${index}. ${fileName} ================== ${fps.original} ==== ${fps.HDFps} ==== ${fps.SDFps} ==== ${size.bytes}`);
+            const sdSize = await getFileSize(`${baseDir}/sdGif/${fileName.replace('.MOV', '.gif')}`);
+            const hdSize = await getFileSize(`${baseDir}/hdGif/${fileName.replace('.MOV', '.gif')}`);
+            // convertMp4ToGif(`${fileDir}/${fileName}`, `${baseDir}/sdGif/${fileName}`, fps.SDFps);
+            // convertMp4ToGif(`${fileDir}/${fileName}`, `${baseDir}/hdGif/${fileName}`, fps.HDFps);
 
-            
+            const fileDetails = {};
+            fileDetails["Name"] = fileName;
+            fileDetails["DefaultFps"] = fps.original;
+            fileDetails["HDFps"] = fps.HDFps;
+            fileDetails["SDFps"] = fps.SDFps;
+            fileDetails["OrginalSizeInByte"] = size.bytes;
+            fileDetails["OrginalSizeInMB"] = Number(size.mb.toFixed(2));
+            fileDetails["SDSizeInByte"] = sdSize.bytes;
+            fileDetails["SDSizeInMB"] = Number(sdSize.mb.toFixed(2));
+            fileDetails["HDSizeInByte"] = hdSize.bytes;
+            fileDetails["HDSizeInMB"] = Number(hdSize.mb.toFixed(2));
+
+            result[index] = fileDetails;
+            console.log(result)
+
+            // console.log(`${index}. ${fileName} ================== ${fps.original} ==== ${fps.HDFps} ==== ${fps.SDFps} ==== ${size.bytes}`);
 
         })
     } catch (error) {
@@ -30,4 +51,5 @@ const mainFunction = async () => {
     }
 };
 
-mainFunction();
+await mainFunction();
+
